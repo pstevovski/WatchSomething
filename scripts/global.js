3a471eabf02e6b6b9173c1be30815aec10af8fdc
Petar Stevovski <stevovski.petar@gmail.com>
2018-09-17 02:20:58 +0200

    Added option to show instant random movie or tv show.

--- scripts/global.js
+++ scripts/global.js
@@ -14,30 +14,31 @@ window.onscroll = function scrollFunction(){
 	
     if(window.pageYOffset >= 30){
 		navbar.style.top = "0px"
-		if(submenu){
-			submenu.style.boxShadow = "0px 0px 15px rgba(0,0,0,0.10)";
-			submenu.style.background = "#f9f9f9";
-		}
+		navbar.style.boxShadow = "0px 3px 5px rgba(0,0,0,0.1)";
     } else {
-		if(submenu){
-			submenu.style.boxShadow = "none";
-			submenu.style.background = "#f9f9f9";
-		}
+		navbar.style.boxShadow = "none";
 	}
 }
 
 // Burger Menu.
 const burger = document.querySelector(".burger");
 const slide = document.querySelector(".slide");
+let sideMenu = false;
+const body = document.querySelector("body");
 burger.addEventListener("click", ()=>{
 	burger.classList.toggle("active");
-
+	
 	// Slide menu.
 	slide.classList.toggle("slideIn");
+	sideMenu = !sideMenu; // Change the flag variable from "false" to "true" or "true" to "false", to be used in the if statement for hiding/showing the scroll button.
 
 	// Fixate body
-	const body = document.querySelector("body");
 	body.classList.toggle("bodyHidden");
+
+	// Hide scroll to top button.
+	if(sideMenu) {
+		scrollToTop.classList.remove("scrollButtonActive");
+	}
 });
 // Click on DROPDOWN - MOVIES on small screen to display/hide the list.
 const smallMovies = document.getElementById("smallScreenMovies");
@@ -68,15 +69,171 @@ scrollToTop.addEventListener("click", ()=>{
 // Modal box and question mark.
 let questionMark = document.getElementById("questionMark");
 let modal = document.querySelector(".modal");
-questionMark.addEventListener("click", ()=>{
-	modal.classList.add("modalActive");
-})
-document.getElementById("modalGotIt").addEventListener("click", ()=>{
-	modal.classList.remove("modalActive");
-})
+if(questionMark){
+	questionMark.addEventListener("click", ()=>{
+		modal.classList.add("modalActive");
+	})	
+}
+const modalGotIt = document.getElementById("modalGotIt");
+if(modalGotIt){
+	modalGotIt.addEventListener("click", ()=>{
+		modal.classList.remove("modalActive");
+	})
+}
 // Close modal with escape key.
 document.body.addEventListener("keydown", (e)=>{
 	if( e.code === "Escape") {
 		modal.classList.remove("modalActive");
 	}
 })
+
+// Random movie / Random TV Show
+const randomMovie = document.querySelector("#randomMovie");
+const randomContainer = document.querySelector(".randomContainer");
+// If randomMovie element exsists..
+if(randomMovie){
+	randomMovie.addEventListener("click", ()=>{
+	// Run random movie function.
+	getRandomMovie();
+	})
+}
+const randomMovieSmall = document.querySelector("#randomMovieSmall");
+if(randomMovieSmall){
+	randomMovieSmall.addEventListener("click", ()=>{
+		// Toggle (off) the burger class and the slide menu class for small screens.
+		burger.classList.toggle("active");
+		slide.classList.toggle("slideIn");
+		body.classList.toggle("bodyHidden");
+		// Run the function
+		getRandomMovie();
+	})
+}
+function getRandomMovie(){
+	// Display the random container.
+	randomContainer.style.display = "block";
+
+	// Get a random page for the API search.
+	let minPage = 1;
+    let maxPage = 20;
+    minPage = Math.ceil(minPage);
+    maxPage = Math.floor(maxPage);
+	let randomPage = Math.floor(Math.random() * (maxPage - minPage + 1)) + minPage;
+
+	// Get a random page for the response result.
+	let minResult = 1;
+	let maxResult = 20;
+	minResult = Math.ceil(minResult);
+	maxResult = Math.floor(maxResult);
+	let randomResult = Math.floor(Math.random() * (maxResult - minResult +1)) + minResult;
+	
+	// Make an api call for a random movie.
+	axios.get("https://api.themoviedb.org/3/movie/top_rated?api_key="+API_KEY+'&language=en-US&page='+randomPage)
+		.then( (response) =>{
+			const movie = response.data.results[randomResult];
+			const output = 
+			`<div class="smallCard">
+			<h2>${movie.title}</h2>
+			<div class="smallMovie">
+					<div class="smallCard_img">
+						<img src="http://image.tmdb.org/t/p/w185/${movie.poster_path}" onerror="this.onerror=null;this.src='../images/imageNotFound.png';">
+					</div>
+					<div class="smallMovie_info">
+						<p id="p_rating"><strong>Rating:</strong> <span>${movie.vote_average} / 10</span> </p>
+						<p><strong>Release date:</strong> <span>${movie.release_date}</span></p>
+						<a onclick="movieSelected('${movie.id}')" href="#">Details</a>
+						<a class="reloadRandom" href="#" onclick="reloadRandomMovie()"><i class="material-icons">autorenew</i></a>
+						<div class="closeSmallCard"><i class="material-icons">close</i></div>
+					</div>
+			</div>
+			</div>`;
+			randomContainer.innerHTML = output;
+			const closeSmallCard = document.querySelector(".closeSmallCard");
+			closeSmallCard.addEventListener("click", ()=> { randomContainer.style.display = "none"});
+		})
+}
+// Details page for the random movie / tv show.
+function movieSelected(id){
+	sessionStorage.setItem("movieId", id);
+	window.open("../movie-page.html");
+	return false;
+}
+// Get another random movie.
+function reloadRandomMovie(){
+	getRandomMovie();
+}
+// Random movie / Random TV Show
+const randomTvShow = document.querySelector("#randomTvShow");
+
+// If randomTvShow element exsist..
+if(randomTvShow) {
+	randomTvShow.addEventListener("click", ()=>{
+	// Run random tv show function.
+	getRandomTvShow();
+	})
+}
+// For small screens.
+const randomTvShowSmall = document.querySelector("#randomTvShowSmall")
+if(randomTvShowSmall){
+	randomTvShowSmall.addEventListener("click", ()=>{
+		// Toggle (off) the burger class and the slide menu class for small screens.
+		burger.classList.toggle("active");
+		slide.classList.toggle("slideIn");
+		body.classList.toggle("bodyHidden");
+		// Run the function.
+		getRandomTvShow();
+	})
+}
+function getRandomTvShow(){
+	// Display the random container.
+	randomContainer.style.display = "block";
+
+	// Get a random page for the API search.
+	let minPage = 1;
+    let maxPage = 20;
+    minPage = Math.ceil(minPage);
+    maxPage = Math.floor(maxPage);
+	let randomPage = Math.floor(Math.random() * (maxPage - minPage + 1)) + minPage;
+
+	// Get a random page for the response result.
+	let minResult = 1;
+	let maxResult = 20;
+	minResult = Math.ceil(minResult);
+	maxResult = Math.floor(maxResult);
+	let randomResult = Math.floor(Math.random() * (maxResult - minResult +1)) + minResult;
+	axios.get("https://api.themoviedb.org/3/tv/top_rated?api_key="+API_KEY+'&language=en-US&page='+randomPage)
+		.then((response)=>{
+			console.log(response);
+			const show = response.data.results[randomResult];
+			const output = 
+			`<div class="smallCard">
+			<h2>${show.name}</h2>
+			<div class="smallMovie">
+					<div class="smallCard_img">
+						<img src="http://image.tmdb.org/t/p/w185/${show.poster_path}" onerror="this.onerror=null;this.src='../images/imageNotFound.png';">
+					</div>
+					<div class="smallMovie_info">
+						<p id="p_rating"><strong>Rating:</strong> <span>${show.vote_average} / 10</span> </p>
+						<p><strong>Release date:</strong> <span>${show.first_air_date}</span></p>
+						<a onclick="showSelected('${show.id}')" href="#">Details</a>
+						<a class="reloadRandom" href="#" onclick="reloadRandomTvShow()"><i class="material-icons">autorenew</i></a>
+						<div class="closeSmallCard"><i class="material-icons">close</i></div>
+					</div>
+			</div>
+			</div>`;
+			randomContainer.innerHTML = output;
+			const closeSmallCard = document.querySelector(".closeSmallCard");
+			closeSmallCard.addEventListener("click", ()=> { randomContainer.style.display = "none"});
+		})
+}
+
+// Go to detailed info page for the tv show.
+function showSelected(id) {
+	sessionStorage.setItem("showId", id);
+	window.open("../shows-page.html");
+	return false;
+}
+
+// Get another random tv show.
+function reloadRandomTvShow(){
+	getRandomTvShow();
+}
